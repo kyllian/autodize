9f194f4140bae6fe95628816dc90671c3bf93ebd
Kyllian Mobley <kyllian@users.noreply.github.com>
2025-04-18 20:35:22 -0500

    Add version 0 of the app

---
+++ Autodize/Program.cs
@@ -0,0 +1,38 @@
+using MudBlazor.Services;
+using Autodize.Client.Pages;
+using Autodize.Components;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Autodize.Client._Imports).Assembly);
+
+app.Run();
